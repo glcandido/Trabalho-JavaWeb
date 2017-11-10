09b298cc19dd06c44ade54538db34f2186da3dcf
glcandido <33287269+glcandido@users.noreply.github.com>
2017-11-10 14:51:15 -0200

    Update CadastroMonitorController.java

--- Grupo3/src/br/com/vidanapratica/escola/controller/CadastroMonitorController.java
+++ Grupo3/src/br/com/vidanapratica/escola/controller/CadastroMonitorController.java
@@ -7,35 +7,78 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import br.com.vidanapratica.escola.helper.Mensagem;
+
 /**
- * Servlet implementation class CadastroMonitorController
+ * Servlet implementation class CadastroComputadorController
  */
-@WebServlet("/CadastroMonitorController")
+@WebServlet("/CadastroComputadorController")
 public class CadastroMonitorController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CadastroMonitorController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		try {
+			// Guardando os valores solicitados em um variáveis
+			String paramId = request.getParameter("id");
+			String paramMarca = request.getParameter("Marca");
+			String paramModelo = request.getParameter("Modelo");
+			String paramGarantia = request.getParameter("Garantia");
+			
+			// Gravando os valores obtidos na requisição.
+			request.setAttribute("paramId", paramId);
+			request.setAttribute("paramMarca", paramMarca);
+			request.setAttribute("paramModelo", paramModelo);
+			request.setAttribute("paramGarantia", paramGarantia);
+			
+			Long id = null;
+			Date garantia = null;
+			
+			// Verificação e validação dos dados informados
+			
+			if(paramId != null && !paramId.trim().equals("") && !paramId.equals("null")){
+				id = Long.valueOf(paramId);
+			}
+			
+			StringBuilder textoDaMensagem = new StringBuilder();
+			
+			if(paramMarca != null && !paramMarca.trim().equals("") && !paramMarca.equals("null")) {
+				textoDaMensagem.append("A marca do computador não foi informada.").append("<br />");
+			}
+			
+			if(paramModelo !=null && !paramModelo.trim().equals("") && !paramModelo.equals("null")) {
+				textoDaMensagem.append("O modelo do computador não foi informado.").append("<br/>");
+			}
+			
+			if(paramGarantia !=null && !paramGarantia.trim().equals("") && !paramGarantia.equals("null")) {
+				textoDaMensagem.append("Informe até quando o produto tem garantia.").append("<br/>");
+			} else {
+				DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+				format.setLenient(false);
+				try {
+					garantia = format.parse(paramGarantia);
+				} catch (ParseException e) {
+					textoDaMensagem.append("Data com formato inválido.").append("<br />");
+				}
+			}
+			
+			Mensagem mensagem = new Mensagem(false,"Operação efetuada com sucesso.");
+			request.setAttribute("mensagem", mensagem);
+			request.getRequestDispatcher("Monitor.jsp").forward(request, response);
+			
+			// Tratamento de exceção
+		} catch (Exception e) {
+			e.printStackTrace();
+			Mensagem mensagem = new Mensagem(true, "Problemas ao salvar o cliente.");
+			request.setAttribute("mensagem", mensagem);
+			request.getRequestDispatcher("Monitor.jsp").forward(request, response);
+		}
+		
 	}
 
 }
